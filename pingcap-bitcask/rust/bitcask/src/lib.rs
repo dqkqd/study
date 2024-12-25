5631f54d6b1070af2452f58eaf4e7ed47cc7e01c
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-12-25 21:57:10 +0900

    feat: add bitcask rust

---
+++ pingcap-bitcask/rust/bitcask/src/lib.rs
@@ -0,0 +1,18 @@
+// just leave it empty for now
+
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        unimplemented!()
+    }
+    pub fn set(&self, key: String, value: String) {
+        unimplemented!()
+    }
+    pub fn get(&self, key: String) -> Option<String> {
+        unimplemented!()
+    }
+    pub fn remove(&self, key: String) {
+        unimplemented!()
+    }
+}
