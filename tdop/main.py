08ad80dd6896776730cf99a058933b0ec4757253
Khanh Duong <dqkqdlot@gmail.com>
2026-02-14 17:01:10 +0900

    remove typer

--- tdop/main.py
+++ tdop/main.py
@@ -1,2 +1,11 @@
+import sys
+import tdop
+
+
 def main(program: str):
-    print(program)
+    expr = tdop.parse(program)
+    print(expr.evalulate())
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
